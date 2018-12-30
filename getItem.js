6529f46f33f84bb72f23625b3bc3bc63db35e4ec
ToLearnJS <44892001+rwj1222@users.noreply.github.com>
2018-12-30 19:12:10 +0800

    Delete getItem.js

--- getItem.js
+++
@@ -1,9 +0,0 @@
-function getItem(div,list){
-    var r=new Array();
-    for (var i=1;i<div.children.length;i++){
-        if (div.children[i].checked==true){
-            r.push(list[i-1]["text"]);
-        }
-    }
-    return r;
-}
